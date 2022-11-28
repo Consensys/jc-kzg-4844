e5c39378253376e89d98934771f9cf76408e7bbf
Stefan Bratanov <stefan.bratanov93@gmail.com>
2022-11-28 12:53:20 +0000

    Initial commit + submodule

---
+++ src/test/bash/checkResources.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -euo pipefail
+
+JAR=${1:?Must specify path to built jc-kzg-4844 jar file}
+
+CONTENTS=$(jar tvf ${JAR})
+
+EXPECTED="ethereum/ckzg4844/lib/libckzg4844jni.so
+ethereum/ckzg4844/lib/ckzg4844jni.dll
+ethereum/ckzg4844/lib/libckzg4844jni.dylib"
+
+EXIT_CODE=0
+
+for LIB in $EXPECTED; do
+  echo -n "Checking for $LIB: "
+  if [[ "$CONTENTS" == *"$LIB"* ]]; then
+    echo "Present"
+  else
+    echo "Missing!"
+    EXIT_CODE=1
+  fi
+done
+
+exit $EXIT_CODE
