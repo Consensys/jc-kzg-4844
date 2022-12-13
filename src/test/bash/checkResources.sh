224b0902dd65b9156285e0f5a143f443651d011e
Stefan Bratanov <stefan.bratanov93@gmail.com>
2022-12-13 13:43:00 +0200

    fix script

--- src/test/bash/checkResources.sh
+++ src/test/bash/checkResources.sh
@@ -8,7 +8,8 @@ JAR=${1:?Must specify path to an already built jc-kzg-4844 jar file}
 
 CONTENTS=$(jar tvf "${JAR}")
 
-EXPECTED="ethereum/ckzg4844/CKZG4844JNI.class
+EXPECTED="ethereum/ckzg4844/CKZG4844JNI\$Preset.class
+ethereum/ckzg4844/CKZG4844JNI.class
 ethereum/ckzg4844/lib/amd64/mainnet/libckzg4844jni.so
 ethereum/ckzg4844/lib/aarch64/mainnet/libckzg4844jni.so
 ethereum/ckzg4844/lib/amd64/mainnet/ckzg4844jni.dll
@@ -20,7 +21,7 @@ ethereum/ckzg4844/lib/amd64/minimal/ckzg4844jni.dll
 ethereum/ckzg4844/lib/x86_64/minimal/libckzg4844jni.dylib
 ethereum/ckzg4844/lib/aarch64/minimal/libckzg4844jni.dylib"
 
-EXPECTED_TOTAL_FILES=11
+EXPECTED_TOTAL_FILES=12
 
 EXIT_CODE=0
 
@@ -35,11 +36,10 @@ for LIB in $EXPECTED; do
 done
 
 if [[ $EXIT_CODE -eq 0 ]]; then
-  echo -n "Checking if the count of the files in the jar is ${EXPECTED_TOTAL_FILES}"
   # exclude directories and the manifest file
   TOTAL_FILES=$(echo "${CONTENTS}" | grep -Ecv "(/|MANIFEST.MF)$")
   if [ "$TOTAL_FILES" -ne $EXPECTED_TOTAL_FILES ]; then
-    echo -n "Number of files in the jar is ${TOTAL_FILES}"
+    echo -e "Expected number of files in the jar to be ${EXPECTED_TOTAL_FILES}, but it was ${TOTAL_FILES}."
     EXIT_CODE=1
   fi
 fi
